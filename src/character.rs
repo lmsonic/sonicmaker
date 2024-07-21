6e41a28b2b2ff0fde05d1f2043d31afb2497beed
Iride Massidda <lorenzomassidda@hotmail.com>
2024-07-21 23:03:20 +0200

    feat: export settings for values

--- src/character.rs
+++ src/character.rs
@@ -1,15 +1,15 @@
 use godot::{
-    engine::{CollisionShape2D, RectangleShape2D},
+    engine::{CollisionShape2D, RectangleShape2D, Shape2D},
     prelude::*,
 };
 #[derive(GodotClass)]
 #[class(tool,init, base=CharacterBody2D)]
 pub struct Character {
-    #[export]
+    #[export(range = (0.0,100.0, 1.0))]
     #[var(get, set = set_width_radius)]
     #[init(default = 19.0)]
     width_radius: f32,
-    #[export]
+    #[export(range = (0.0,100.0, 1.0))]
     #[var(get, set = set_height_radius)]
     #[init(default = 39.0)]
     height_radius: f32,
@@ -20,7 +20,7 @@ pub struct Character {
     #[init(default = 6.5)]
     jump_force: f32,
     #[export]
-    shape: Option<Gd<CollisionShape2D>>,
+    collision_shape: Option<Gd<CollisionShape2D>>,
 }
 
 #[godot_api]
@@ -28,22 +28,30 @@ impl Character {
     #[func]
     fn set_width_radius(&mut self, value: f32) {
         self.width_radius = value;
-        if let Some(shape) = self.shape.as_deref().and_then(|shape| shape.get_shape()) {
-            if let Ok(mut rectangle) = shape.try_cast::<RectangleShape2D>() {
-                let y = rectangle.get_size().y;
-                rectangle.set_size(Vector2::new(self.width_radius, y));
-            }
-        }
+        let Some(mut rectangle) = self.get_rectangle() else {
+            return;
+        };
+
+        let y = rectangle.get_size().y;
+        rectangle.set_size(Vector2::new(self.width_radius, y));
     }
 
     #[func]
     fn set_height_radius(&mut self, value: f32) {
         self.height_radius = value;
-        if let Some(shape) = self.shape.as_deref().and_then(|shape| shape.get_shape()) {
-            if let Ok(mut rectangle) = shape.try_cast::<RectangleShape2D>() {
-                let x = rectangle.get_size().x;
-                rectangle.set_size(Vector2::new(x, self.height_radius));
-            }
-        }
+        let Some(mut rectangle) = self.get_rectangle() else {
+            return;
+        };
+
+        let x = rectangle.get_size().x;
+        rectangle.set_size(Vector2::new(x, self.height_radius));
+    }
+
+    fn get_rectangle(&self) -> Option<Gd<RectangleShape2D>> {
+        self.collision_shape
+            .as_deref()?
+            .get_shape()?
+            .try_cast()
+            .ok()
     }
 }
