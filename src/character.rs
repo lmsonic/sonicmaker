c87ff8644c0d6beed35af9de44d8ab749dbce0a0
Iride Massidda <lorenzomassidda@hotmail.com>
2024-07-24 20:03:38 +0200

    feat: account for velocity in landing

--- src/character.rs
+++ src/character.rs
@@ -78,18 +78,39 @@ impl Mode {
     #[allow(clippy::just_underscores_and_digits)]
     fn from_ground_angle(angle: f32) -> Self {
         let _0 = f32::to_radians(0.0);
-        let _45 = f32::to_radians(45.0);
+        let _46 = f32::to_radians(46.0);
         let _135 = f32::to_radians(135.0);
-        let _224 = f32::to_radians(224.0);
+        let _226 = f32::to_radians(226.0);
         let _315 = f32::to_radians(315.0);
         let _360 = f32::to_radians(360.0);
-        if (_0.._45).contains(&angle) || (_315.._360).contains(&angle) {
+        if (_0.._46).contains(&angle) || (_315.._360).contains(&angle) {
             Self::Floor
-        } else if (_45.._135).contains(&angle) {
+        } else if (_46.._135).contains(&angle) {
             Self::RightWall
-        } else if (_135.._224).contains(&angle) {
+        } else if (_135.._226).contains(&angle) {
             Self::Ceiling
-        } else if (_224.._360).contains(&angle) {
+        } else if (_226.._315).contains(&angle) {
+            Self::LeftWall
+        } else {
+            godot_warn!("out of range 0-360 angle {angle}");
+            Self::default()
+        }
+    }
+    #[allow(clippy::just_underscores_and_digits)]
+    fn from_wall_angle(angle: f32) -> Self {
+        let _0 = f32::to_radians(0.0);
+        let _45 = f32::to_radians(45.0);
+        let _136 = f32::to_radians(136.0);
+        let _225 = f32::to_radians(225.0);
+        let _316 = f32::to_radians(316.0);
+        let _360 = f32::to_radians(360.0);
+        if (_0.._45).contains(&angle) || (_316.._360).contains(&angle) {
+            Self::Floor
+        } else if (_45.._136).contains(&angle) {
+            Self::RightWall
+        } else if (_136.._225).contains(&angle) {
+            Self::Ceiling
+        } else if (_225.._316).contains(&angle) {
             Self::LeftWall
         } else {
             godot_warn!("out of range 0-360 angle {angle}");
@@ -353,6 +374,7 @@ impl Character {
     fn should_snap_to_floor(&self, result: DetectionResult) -> bool {
         // Sonic 1
         result.distance > -14.0 && result.distance < 14.0
+        // TODO: use this accounting for speed
         // Sonic 2 and onwards
         // let mode = Mode::from_normal(result.normal);
         // let velocity = self.base().get_velocity();
@@ -363,10 +385,55 @@ impl Character {
         //     distance <= (velocity.y.abs() + 4.0).min(14.0) && distance >= -14.0
         // }
     }
-    fn is_landed(&self, result: DetectionResult) -> bool {
+    fn is_landed(&mut self, result: DetectionResult) -> bool {
+        enum MotionDirection {
+            Right,
+            Up,
+            Left,
+            Down,
+        }
+
+        impl MotionDirection {
+            #[allow(clippy::just_underscores_and_digits)]
+            fn from_velocity_angle(angle: f32) -> Self {
+                let _0 = f32::to_radians(0.0);
+                let _46 = f32::to_radians(46.0);
+                let _136 = f32::to_radians(136.0);
+                let _226 = f32::to_radians(226.0);
+                let _316 = f32::to_radians(316.0);
+                let _360 = f32::to_radians(360.0);
+                if (_0.._46).contains(&angle) || (_316.._360).contains(&angle) {
+                    Self::Right
+                } else if (_46.._136).contains(&angle) {
+                    Self::Up
+                } else if (_136.._226).contains(&angle) {
+                    Self::Down
+                } else if (_226.._316).contains(&angle) {
+                    Self::Left
+                } else {
+                    godot_warn!("out of range 0-360 angle {angle}");
+                    Self::Down
+                }
+            }
+        }
         result.distance.abs() < 4.0
+        // TODO: use this once we have a controller
+        // if result.distance > 0.0 {
+        //     return false;
+        // }
+        // let velocity = self.base().get_velocity();
+        // let motion_angle = velocity.angle_0_360();
+        // let direction = MotionDirection::from_velocity_angle(motion_angle);
+        // match direction {
+        //     MotionDirection::Right | MotionDirection::Left => self
+        //         .ground_sensor_results()
+        //         .iter()
+        //         .any(|r| r.distance >= -velocity.y + 8.0),
+        //     MotionDirection::Down => velocity.y >= 0.0,
+        //     MotionDirection::Up => false,
+        // }
     }
-    fn ground_check(&mut self) -> Option<DetectionResult> {
+    fn ground_sensor_results(&mut self) -> Vec<DetectionResult> {
         let mut results = vec![];
         if let Some(sensor_a) = &mut self.sensor_floor_left {
             if let Ok(r) = sensor_a
@@ -385,8 +452,11 @@ impl Character {
             {
                 results.push(r);
             }
-        };
+        }
         results
+    }
+    fn ground_check(&mut self) -> Option<DetectionResult> {
+        self.ground_sensor_results()
             .into_iter()
             .min_by(|a, b| a.distance.abs().total_cmp(&b.distance.abs()))
     }
