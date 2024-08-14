59800a231cab3065e99edc3c16265dbf117957ba
Iride Massidda <lorenzomassidda@hotmail.com>
2024-08-14 21:58:02 +0200

    feat: ring sprites

--- godot/hitboxes/increment_rings.gd
+++ godot/hitboxes/increment_rings.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
 @export var amount := 1
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+var collected:=false
 func _on_area_entered(area: Area2D) -> void:
+	if collected:
+		return
 	var player_hitbox := area as PlayerHitbox
 	if player_hitbox:
 		player_hitbox.increment_rings(amount)
+		sprite.play("collected")
+		collected = true
+		await sprite.animation_finished
 		queue_free()
+
