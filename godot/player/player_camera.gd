5d0b65ac04bd480074694354d3c16ce3ed29ba49
Iride Massidda <lorenzomassidda@hotmail.com>
2024-09-03 15:14:34 +0200

    feat: pixel perfect camera

--- godot/player/player_camera.gd
+++ godot/player/player_camera.gd
@@ -4,15 +4,23 @@ extends Camera2D
 
 const air_vertical_margin := 0.14285714285
 
+static func move_toward_positioni(origin: Vector2, target: Vector2, speed: float) -> Vector2:
+	target = floor(target)
+	origin = floor(origin)
+	return Vector2(move_toward(target.x,origin.x,speed),move_toward(target.y,origin.y,speed))
+
+
+
+
+
 func _physics_process(delta: float) -> void:
 	if player.is_grounded:
 		drag_top_margin = 0.0
 		drag_bottom_margin = 0.0
-		#if absf(player.ground_speed) >= 8.0:
-			#position_smoothing_speed = 24.0
-		#else:
-			#position_smoothing_speed = 6.0
+		position_smoothing_speed = 24.0 if absf(player.ground_speed) >= 8.0 else 6.0
+
 	else:
 		drag_top_margin = air_vertical_margin
 		drag_bottom_margin = air_vertical_margin
-		#position_smoothing_speed = 24.0
+		position_smoothing_speed = 24
+	position = move_toward_positioni(position,offset,position_smoothing_speed)
